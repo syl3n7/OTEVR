bd27478757a7ef312403f9d21bd4a9d9ce5d7b9a
Syl3nt ~ Cláudio <claudio.silva365@gmail.com>
2024-01-07 18:30:16 +0000

    we can grab things now !

--- Assets/ActivateTeleportationRay.cs
+++ Assets/ActivateTeleportationRay.cs
@@ -7,15 +7,12 @@ public class ActivateTeleportationRay : MonoBehaviour
 {
 
     public GameObject leftTeleportation;
-    public GameObject rightTeleportation;
 
     public InputActionProperty leftActivate;
-    public InputActionProperty rightActivate;
 
     void Update()
     {
         leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
     }
 
 }
