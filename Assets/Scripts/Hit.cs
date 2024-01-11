3962a875083649d1eb535ee43bf5dc455523f60d
syl3n7 <a040516@ipmaia.pt>
2024-01-11 19:00:01 +0000

    Added a few changes to make the experience smoother

---
+++ Assets/Scripts/Hit.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Hit : MonoBehaviour
+{
+    [SerializeField] private GameObject counter;
+    [SerializeField] private GameObject v;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Target")
+        {
+            ContactPoint contact = collision.contacts[0];
+            Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 position = contact.point;
+            Instantiate(v, position, rotation);
+            counter.GetComponent<pointCounter>().AddPoints();
+            Destroy(gameObject, 0.3f);
+        }
+    }
+}
---
+++ Assets/Scripts/pointsCounter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+using UnityEngine.VFX;
+using TMPro;
+
+public class pointCounter : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+    private int points = 0;
+
+    public void AddPoints()
+    {
+        points++;
+    }
+
+    private void Update()
+    {
+        text.text = "Points: " + points;
+    }
+}
